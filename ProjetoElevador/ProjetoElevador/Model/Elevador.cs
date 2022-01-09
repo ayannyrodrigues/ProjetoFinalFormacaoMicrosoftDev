d1ac31217ba4d73bb80eecbde729af851dabdb00
ayanny <ayanny_maria@hotmail.com>
2022-01-09 10:05:56 -0300

    commit inicial

---
+++ ProjetoElevador/ProjetoElevador/Model/Elevador.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+{
+    class Elevador
+    {
+        // Atributos da classe 
+        public int andarAtual { get; set; }
+        public int totalAndares { get; set; }
+        public int capacidadeElevador { get; set; }
+        public int qntdPessoas { get; set; }
+
+
+        // Construtor
+        public Elevador(int totalAndares, int capacidadeElevador)
+        {
+            this.andarAtual = 0;
+            this.totalAndares = totalAndares;
+            this.capacidadeElevador = capacidadeElevador;
+            this.qntdPessoas = 0;
+        }
+
+
+        // Métodos
+        public int Entrar()
+        {
+            if (qntdPessoas < capacidadeElevador)
+            {
+                qntdPessoas++;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("O elevador está cheio. Por gentileza, aguarde o próximo.");
+                Console.ResetColor();
+            }
+
+            return qntdPessoas;
+        }
+
+        // Remove uma pessoa do elevador
+        public int Sair()
+        {
+            if (qntdPessoas != 0)
+            {
+                qntdPessoas--;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("O elevador está vazio.");
+                Console.ResetColor();
+            }
+
+            return qntdPessoas;
+        }
+
+        // Sobe um andar 
+        public int Subir()
+        {
+            if (andarAtual < totalAndares)
+            {
+                andarAtual++;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Você chegou ao último andar, não é possível subir.");
+                Console.ResetColor();
+            }
+
+            return andarAtual;
+        }
+
+        // Desce um andar
+        public int Descer()
+        {
+            if (andarAtual != 0)
+            {
+                andarAtual--;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Você chegou ao terréo, não é possível descer.");
+                Console.ResetColor();
+            }
+
+            return andarAtual;
+        }
+
+
+        // Reescreve o método ToString
+        public override string ToString()
+        {
+            return "\nAndar atual: " + andarAtual + "º" + "\n" + "Quantidade de pessoas dentro do elevador: " + qntdPessoas;
+
+        }
+    }
+}
---
+++ ProjetoElevador/ProjetoElevador/Program.cs
@@ -0,0 +1,70 @@
+﻿using ProjetoElevador.Model;
+using System;
+
+namespace ProjetoElevador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("BEM-VINDO (A) AO SIMULADOR DE ELEVADOR");
+            Console.WriteLine("------------------------------------\n");
+            Console.ResetColor();
+
+            // Valores informados pelo usuário
+            Console.Write("Informe o total de andares do prédio: ");
+            int andares = int.Parse(Console.ReadLine());
+            Console.Write("Informe a capacidade do elevador: ");
+            int capacidade = int.Parse(Console.ReadLine());
+
+            // Instanciação de um objeto da classe Elevador
+            Elevador elevador = new Elevador(andares, capacidade);
+
+            bool ativo = true;
+
+            // Estrutura de repetição que irá primeiro apresentar na tela as ações do menu e depois verificar se a variável "ativo" é verdadeira, caso seja irá repetir 
+            do
+            {
+                Console.WriteLine(elevador);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\nEscolha uma opção: 1-[Entrar]  2-[Sair]  3-[Subir]  4-[Descer]  5-[Encerrar]");
+                Console.ResetColor();
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        elevador.Entrar();
+                        break;
+
+                    case "2":
+                        elevador.Sair();
+                        break;
+
+                    case "3":
+                        elevador.Subir();
+                        break;
+
+                    case "4":
+                        elevador.Descer();
+                        break;
+
+                    case "5":
+                        ativo = false;
+                        Console.WriteLine("O sistema será encerrado. Obrigada por utilizar o simulador de elevador.");
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Opção inválida! Digite uma opção válida no menu.");
+                        Console.ResetColor();
+                        break;
+                }
+
+            } while (ativo);
+
+        }
+    }
+}
